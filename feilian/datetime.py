b6a1f8005ff2bead742bd7e22256b45b71c29c32
darkpeath <darkpeath@gmail.com>
2023-08-13 17:23:45 +0800

    fix: fix a typo in datetime tool

--- feilian/datetime.py
+++ feilian/datetime.py
@@ -2,18 +2,19 @@
 
 from typing import Union
 import pandas as pd
+import datetime
 
-def format_time(time: Union[str, int, float, feilian.datetime] = None, fmt='%Y-%m-%d %H:%M:%S') -> str:
+def format_time(time: Union[str, int, float, datetime.datetime] = None, fmt='%Y-%m-%d %H:%M:%S') -> str:
     if time is None:
-        time = feilian.datetime.now()
+        time = datetime.datetime.now()
     elif isinstance(time, (int, float)):
-        time = feilian.datetime.fromtimestamp(time)
+        time = datetime.datetime.fromtimestamp(time)
     elif isinstance(time, str):
         time = pd.to_datetime(time)
     else:
-        if not isinstance(time, feilian.datetime):
+        if not isinstance(time, datetime.datetime):
             raise ValueError(f"Unexpected type: {type(time)}")
     return time.strftime(fmt)
 
-def format_date(date: Union[str, int, float, feilian.datetime] = None, sep='-') -> str:
+def format_date(date: Union[str, int, float, datetime.datetime] = None, sep='-') -> str:
     return format_time(date, fmt=sep.join(['%Y', '%m', '%d']))
