41b460ee0a7432e75ffa04d91ce4711a8bc56984
darkpeath <darkpeath@gmail.com>
2023-08-15 20:25:59 +0800

    perf: add text check function to root module

--- feilian/__init__.py
+++ feilian/__init__.py
@@ -2,6 +2,7 @@
 
 from .io import ensure_parent_dir_exist
 from .dataframe import read_dataframe, save_dataframe, extract_dataframe_sample, merge_dataframe_rows, iter_dataframe
+from .dataframe import is_empty_text, is_nonempty_text, is_blank_text, is_non_blank_text
 from .datetime import format_time, format_date
 from .arg import ArgValueParser
 from .json import read_json, save_json
@@ -10,6 +11,7 @@ from .about import __version__
 __all__ = [
     'ensure_parent_dir_exist',
     'read_dataframe', 'save_dataframe', 'extract_dataframe_sample', 'merge_dataframe_rows', 'iter_dataframe',
+    'is_empty_text', 'is_nonempty_text', 'is_blank_text', 'is_non_blank_text',
     'format_time', 'format_date',
     'ArgValueParser',
     'read_json', 'save_json',
