107cd714b35dc802cde3f98ac79c32cc0ae65d74
darkpeath <darkpeath@gmail.com>
2023-08-24 20:59:49 +0800

    perf: ensure dir exist before write json

--- feilian/json.py
+++ feilian/json.py
@@ -2,6 +2,7 @@
 
 from typing import Dict, List, Union, Any
 import json
+from .io import ensure_parent_dir_exist
 
 def read_json(filepath: str, encoding='utf-8', **kwargs):
     """
@@ -15,5 +16,6 @@ def save_json(filepath: str, data: Union[Dict[str, Any], List[Any]],
     """
     An agent for `json.dump()` with some default value.
     """
+    ensure_parent_dir_exist(filepath)
     with open(filepath, 'w', encoding=encoding, newline=newline) as f:
         json.dump(data, f, indent=indent, ensure_ascii=ensure_ascii, **kwargs)
