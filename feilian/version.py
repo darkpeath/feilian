8cadaf838fabb63dbe83454f5c90a2c284295b8d
darkpeath <darkpeath@gmail.com>
2023-08-28 21:46:49 +0800

    perf: use a compatable module

--- feilian/version.py
+++ feilian/version.py
@@ -3,7 +3,7 @@
 try:
     from ._dist_ver import VERSION, __version__
 except ImportError:
-    from importlib.metadata import version, PackageNotFoundError
+    from importlib_metadata import version, PackageNotFoundError
     try:
         __version__ = version('feilian')
     except PackageNotFoundError:
