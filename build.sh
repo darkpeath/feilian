ac294bd4ef2459d6f83779ded1a5ef0c82c75d3d
darkpeath <darkpeath@gmail.com>
2023-08-28 19:04:06 +0800

    perf: fix commit conflict

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD
 python -m pip install --upgrade pip
 python -m pip install --upgrade build wheel
-=======
-python -m pip install pip
-python -m pip install --upgrade build wheel
-python -m pip install -r requirements.txt
->>>>>>> 2c9fb3bff11a8472a9c5067e915056ed265e72fc
 python -m build
