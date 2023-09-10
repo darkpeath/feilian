c485f417aeabd92a8ec78ea98dc9537bbdf79aca
darkpeath <darkpeath@gmail.com>
2023-09-10 21:42:52 +0800

    feat: compatible with jsonl read and write

--- feilian/json.py
+++ feilian/json.py
@@ -4,18 +4,29 @@ from typing import Dict, List, Union, Any
 import json
 from .io import ensure_parent_dir_exist
 
-def read_json(filepath: str, encoding='utf-8', **kwargs):
+def read_json(filepath: str, jsonl=False, encoding='utf-8', **kwargs):
     """
     An agent for `json.load()` with some default value.
     """
     with open(filepath, encoding=encoding) as f:
-        return json.load(f, **kwargs)
+        if jsonl:
+            return [json.loads(x) for x in f]
+        else:
+            return json.load(f, **kwargs)
 
-def save_json(filepath: str, data: Union[Dict[str, Any], List[Any]],
+def save_json(filepath: str, data: Union[Dict[str, Any], List[Any]], jsonl=False,
               encoding='utf-8', newline='\n', indent=2, ensure_ascii=False, **kwargs):
     """
     An agent for `json.dump()` with some default value.
     """
+    if jsonl and not isinstance(data, list):
+        # data should be a list
+        raise ValueError("data should be a list when save as jsonl format")
     ensure_parent_dir_exist(filepath)
     with open(filepath, 'w', encoding=encoding, newline=newline) as f:
-        json.dump(data, f, indent=indent, ensure_ascii=ensure_ascii, **kwargs)
+        if jsonl:
+            for x in data:
+                f.write(json.dumps(x, ensure_ascii=ensure_ascii, **kwargs))
+                f.write(newline)
+        else:
+            json.dump(data, f, indent=indent, ensure_ascii=ensure_ascii, **kwargs)
