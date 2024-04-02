34498fc26f3f9f34a8b50a60a24b4a1317bc23f0
darkpeath <darkpeath@gmail.com>
2024-04-02 11:14:40 +0800

    perf: add args to control exclude, frozen and empty value

--- feilian/utils.py
+++ feilian/utils.py
@@ -1,27 +1,64 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from typing import Dict, Any
+from typing import Dict, Any, Union, Collection
 
-def flatten_dict(data: Dict[str, Any], prefix="", joiner=".", exclude_prefixes=None,
+def flatten_dict(data: Dict[str, Any], prefix="", joiner=".",
+                 exclude: Union[None, str, Collection[str]] = None,
+                 frozen: Union[None, str, Collection[str]] = None,
+                 empty_as_default=False, empty_value=None,
                  res: Dict[str, Any] = None) -> Dict[str, Any]:
     """
     flatten dict as a flat one layer dict
     :param data:      origin dict
     :param prefix:    prefix for key in the dict
     :param joiner:    join symbol for different layer key
-    :param exclude_prefixes:  keys exclude from result
+    :param exclude:   prefix to be excluded from result
+    :param frozen:    keys not to be flattened
+    :param empty_as_default:  should set a default value if value is an empty dict
+    :param empty_value:       if `empty_as_default` is `True`, used as the default value for empty dict
     :param res:       the result flat layer dict, create a new one if not given.
     """
     if res is None:
         res = {}
-    if exclude_prefixes and prefix in exclude_prefixes:
+    if isinstance(exclude, str):
+        exclude = {exclude}
+    if isinstance(frozen, str):
+        frozen = {frozen}
+
+    # all keys are start with the prefix, ignore data
+    if exclude and prefix in exclude:
         return res
+
+    # all keys in data should be frozen
+    if frozen and prefix in frozen:
+        for k, v in data.items():
+            res[prefix+k] = v
+        return res
+
     for k, v in data.items():
         k = prefix + k
-        if isinstance(v, dict) and k not in exclude_prefixes:
-            flatten_dict(v, prefix=k + joiner, joiner=joiner, exclude_prefixes=exclude_prefixes, res=res)
-        else:
+
+        if exclude and k in exclude:
+            # only the key should be excluded
+            continue
+
+        if frozen and k in frozen:
+            # frozen key, keep it as original value
             res[k] = v
+            continue
+
+        if isinstance(v, dict):
+            if len(v) == 0:
+                # empty dict, set as default value if set
+                if empty_as_default:
+                    res[k] = empty_value
+            else:
+                # value is a dict, flatten recursively
+                flatten_dict(v, prefix=k+joiner, joiner=joiner, exclude=exclude, frozen=frozen, res=res)
+        else:
+            # normal value, keep it as original value
+            res[k] = v
+
     return res
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,7 +13,35 @@ def test_flatten_dict():
                 "se": "we",
                 "t": 5,
             }
-        }
+        },
+        "f": 7,
+        "g": {
+            "ts": "9w",
+            "j2": 8,
+        },
+        "w": {
+            "s": {
+                "ge": 89,
+                "00": "ej",
+            },
+            "r": {
+                "le": 33,
+                "03": "ef",
+            }
+        },
+        "sk": {
+            "a": "23",
+            "b": {
+                "s": 9,
+                "g": 0,
+                "p": 4,
+            },
+            "c": {
+                "s": 8,
+                "t": "w",
+                "j": "23",
+            }
+        },
     }
     d2 = {
         "a": 12,
@@ -21,8 +49,29 @@ def test_flatten_dict():
         "c.l": 0,
         "c.j.se": "we",
         "c.j.t": 5,
+        "g": {
+            "ts": "9w",
+            "j2": 8,
+        },
+        "w.s": {
+            "ge": 89,
+            "00": "ej",
+        },
+        "w.r.le": 33,
+        "w.r.03": "ef",
+        "sk.a": "23",
+        "sk.b": {
+            "s": 9,
+            "g": 0,
+            "p": 4,
+        },
+        "sk.c": {
+            "s": 8,
+            "t": "w",
+            "j": "23",
+        }
     }
 
-    d3 = feilian.flatten_dict(d1)
+    d3 = feilian.flatten_dict(d1, frozen={"g", "w.s", "sk."}, exclude="f")
 
     assert d2 == d3
