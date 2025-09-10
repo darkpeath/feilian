46066e8c0650a982abf7802dda5aa1bbdcee1f1c
darkpeath <darkpeath@gmail.com>
2025-09-10 19:56:07 +0800

    feat: read and write txt file

---
+++ feilian/txt.py
@@ -0,0 +1,16 @@
+from typing import Union, Literal
+import os
+import chardet
+
+def read_txt(path: Union[str, os.PathLike], encoding: Union[None, Literal['auto'], str] = None) -> str:
+    if encoding == 'auto':
+        with open(path, 'rb') as f:
+            raw = f.read()
+        encoding = chardet.detect(raw)['encoding']
+        return raw.decode(encoding)
+    with open(path, 'r', encoding=encoding) as f:
+        return f.read()
+
+def save_txt(path: Union[str, os.PathLike], content: str, encoding: str = 'utf-8'):
+    with open(path, 'w', encoding=encoding) as f:
+        f.write(content)
